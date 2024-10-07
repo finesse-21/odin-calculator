56e82b65d8b9c4bd4be6efe34ecf00c2b9c05d40
finesse-21 <21pepperforreal@gmail.com>
2024-10-07 22:36:08 +0300

    Create function handling all buttons

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-let firstNumber = 0;
-let operand = "";
-let secondNumber = 0;
-let result = 0;
+let firstNumber;
+let operand;
+let secondNumber;
+let result;
+const ALL_OPERANDS = "+-*/"
 
 function operate(firstNumber, operand, secondNumber) {
     if (operand == "+") return firstNumber + secondNumber;
@@ -12,52 +13,77 @@ function operate(firstNumber, operand, secondNumber) {
 
 const displayContainer = document.querySelector(".display");
 
-const digitButton = document.querySelector(".digits");
+const digitButton = document.querySelector(".buttons");
 digitButton.addEventListener("click", (event) => {
-    if (Number.isInteger(parseInt(event.target.innerText))) {
-        displayContainer.textContent += event.target.innerText;
+    calculatorAction(event.target.innerText);
+  });
+
+function calculatorAction(event) {
+    if (Number.isInteger(parseInt(event))) {
+        displayContainer.textContent += event;
     }
-    console.log(event.target.innerText);
-  });
+    if (event == "AC") {
+        firstNumber = NaN;
+        secondNumber = NaN;
+        operand = "";
+        displayContainer.textContent = "";
+    }
+    if (event == "=") {
+        secondNumber = parseInt(displayContainer.textContent);
+        result = operate(firstNumber, operand, secondNumber);
+        displayContainer.textContent = result;
+    }
+    if (ALL_OPERANDS.includes(event)) {
+        if (!firstNumber) {
+            firstNumber = parseInt(displayContainer.textContent);
+        }
+        else {
+            secondNumber = parseInt(displayContainer.textContent);
+            result = operate(firstNumber, operand, secondNumber);
+            firstNumber = result;
+        }
+        displayContainer.textContent = "";
+        operand = event;
+    }
+}
 
+// const clearButton = document.querySelector(".clear")
+// clearButton.addEventListener("click", () => {
+//     displayContainer.textContent = "";
+//   });
 
-const clearButton = document.querySelector(".clear")
-clearButton.addEventListener("click", () => {
-    displayContainer.textContent = "";
-  });
+// const addButton = document.querySelector(".add");
+// addButton.addEventListener("click", () => {
+//     firstNumber = parseInt(displayContainer.textContent);
+//     operand = "+"
+//     displayContainer.textContent = "";
+//   });
 
-const addButton = document.querySelector(".add");
-addButton.addEventListener("click", () => {
-    firstNumber = parseInt(displayContainer.textContent);
-    operand = "+"
-    displayContainer.textContent = "";
-  });
+// const subtractButton = document.querySelector(".subtract");
+// subtractButton.addEventListener("click", () => {
+//     firstNumber = parseInt(displayContainer.textContent);
+//     operand = "-"
+//     displayContainer.textContent = "";
+// });
 
-const subtractButton = document.querySelector(".subtract");
-subtractButton.addEventListener("click", () => {
-    firstNumber = parseInt(displayContainer.textContent);
-    operand = "-"
-    displayContainer.textContent = "";
-});
+// const multiplyButton = document.querySelector(".multiply");
+// multiplyButton.addEventListener("click", () => {
+//     firstNumber = parseInt(displayContainer.textContent);
+//     operand = "*"
+//     displayContainer.textContent = "";
+// });
 
-const multiplyButton = document.querySelector(".multiply");
-multiplyButton.addEventListener("click", () => {
-    firstNumber = parseInt(displayContainer.textContent);
-    operand = "*"
-    displayContainer.textContent = "";
-});
+// const divideButton = document.querySelector(".divide");
+// divideButton.addEventListener("click", () => {
+//     firstNumber = parseInt(displayContainer.textContent);
+//     operand = "/"
+//     displayContainer.textContent = "";
+// });
 
-const divideButton = document.querySelector(".divide");
-divideButton.addEventListener("click", () => {
-    firstNumber = parseInt(displayContainer.textContent);
-    operand = "/"
-    displayContainer.textContent = "";
-});
-
-const equalsButton = document.querySelector(".equals");
-equalsButton.addEventListener("click", () => {
-    secondNumber = parseInt(displayContainer.textContent);
-    result = operate(firstNumber, operand, secondNumber);
-    displayContainer.textContent = result;
-});
+// const equalsButton = document.querySelector(".equals");
+// equalsButton.addEventListener("click", () => {
+//     secondNumber = parseInt(displayContainer.textContent);
+//     result = operate(firstNumber, operand, secondNumber);
+//     displayContainer.textContent = result;
+// });
  
