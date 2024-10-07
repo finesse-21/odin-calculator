59fc00166fecfcdd1dbf9ab2507d43284f3ae820
finesse-21 <21pepperforreal@gmail.com>
2024-10-07 22:00:31 +0300

    Optimize entering digits on display

--- script.js
+++ script.js
@@ -12,55 +12,14 @@ function operate(firstNumber, operand, secondNumber) {
 
 const displayContainer = document.querySelector(".display");
 
-const oneButton = document.querySelector(".one");
-oneButton.addEventListener("click", () => {
-    displayContainer.textContent += "1";
+const digitButton = document.querySelector(".digits");
+digitButton.addEventListener("click", (event) => {
+    if (Number.isInteger(parseInt(event.target.innerText))) {
+        displayContainer.textContent += event.target.innerText;
+    }
+    console.log(event.target.innerText);
   });
 
-const twoButton = document.querySelector(".two");
-twoButton.addEventListener("click", () => {
-    displayContainer.textContent += "2";
-  });
-
-const threeButton = document.querySelector(".three");
-threeButton.addEventListener("click", () => {
-    displayContainer.textContent += "3";
-  });
-
-const fourButton = document.querySelector(".four");
-fourButton.addEventListener("click", () => {
-    displayContainer.textContent += "4";
-  });
-
-const fiveButton = document.querySelector(".five");
-fiveButton.addEventListener("click", () => {
-    displayContainer.textContent += "5";
-  });
-
-const sixButton = document.querySelector(".six");
-sixButton.addEventListener("click", () => {
-    displayContainer.textContent += "6";
-  });
-  
-const sevenButton = document.querySelector(".seven");
-sevenButton.addEventListener("click", () => {
-    displayContainer.textContent += "7";
-  });
-
-const eightButton = document.querySelector(".eight");
-eightButton.addEventListener("click", () => {
-    displayContainer.textContent += "8";
-  });
-
-const nineButton = document.querySelector(".nine");
-nineButton.addEventListener("click", () => {
-    displayContainer.textContent += "9";
-  });
-
-const zeroButton = document.querySelector(".zero");
-zeroButton.addEventListener("click", () => {
-    displayContainer.textContent += "0";
-  });
 
 const clearButton = document.querySelector(".clear")
 clearButton.addEventListener("click", () => {
