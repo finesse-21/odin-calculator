da0d3c884f5e5ce0a9220c4be21f1bd839d31cbf
finesse-21 <21pepperforreal@gmail.com>
2024-10-07 15:36:11 +0300

    Create basic functionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let firstNumber = 0;
 let operand = "";
 let secondNumber = 0;
+let result = 0;
 
 function operate(firstNumber, operand, secondNumber) {
     if (operand == "+") return firstNumber + secondNumber;
@@ -60,3 +61,44 @@ const zeroButton = document.querySelector(".zero");
 zeroButton.addEventListener("click", () => {
     displayContainer.textContent += "0";
   });
+
+const clearButton = document.querySelector(".clear")
+clearButton.addEventListener("click", () => {
+    displayContainer.textContent = "";
+  });
+
+const addButton = document.querySelector(".add");
+addButton.addEventListener("click", () => {
+    firstNumber = parseInt(displayContainer.textContent);
+    operand = "+"
+    displayContainer.textContent = "";
+  });
+
+const subtractButton = document.querySelector(".subtract");
+subtractButton.addEventListener("click", () => {
+    firstNumber = parseInt(displayContainer.textContent);
+    operand = "-"
+    displayContainer.textContent = "";
+});
+
+const multiplyButton = document.querySelector(".multiply");
+multiplyButton.addEventListener("click", () => {
+    firstNumber = parseInt(displayContainer.textContent);
+    operand = "*"
+    displayContainer.textContent = "";
+});
+
+const divideButton = document.querySelector(".divide");
+divideButton.addEventListener("click", () => {
+    firstNumber = parseInt(displayContainer.textContent);
+    operand = "/"
+    displayContainer.textContent = "";
+});
+
+const equalsButton = document.querySelector(".equals");
+equalsButton.addEventListener("click", () => {
+    secondNumber = parseInt(displayContainer.textContent);
+    result = operate(firstNumber, operand, secondNumber);
+    displayContainer.textContent = result;
+});
+ 
